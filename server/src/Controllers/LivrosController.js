a366d1a9382ed6b4620283ec1d92fb404432e47a
LeoBasso <127260629+LeoBasso@users.noreply.github.com>
2023-11-28 22:39:11 -0300

    200

--- server/src/Controllers/LivrosController.js
+++ server/src/Controllers/LivrosController.js
@@ -5,14 +5,14 @@ const Livros = require('../Model/Livros');
 class LivrosController {
   async getAllLivros(req, res) {
     const livros = await Livros.find();
-    res.status(StatusCodes.OK).json(livros);
+    res.status(200).json(livros);
   }
 
   async createLivros(req, res) {
     const livrosData = req.body;
     console.log(livrosData);
     const newLivro = await Livros.create(livrosData);
-    res.status(StatusCodes.CREATED).json(newLivro);
+    res.status(200).json(newLivro);
   }
 
   async updateLivros(req, res) {
@@ -22,13 +22,13 @@ class LivrosController {
       new: true,
       runValidators: true,
     });
-    res.status(StatusCodes.OK).json(updatedLivro);
+    res.status(201).json(updatedLivro);
   }
 
   async deleteLivros(req, res) {
     const id = req.params.id;
     await Livros.findByIdAndDelete(id);
-    res.sendStatus(StatusCodes.NO_CONTENT);
+    res.sendStatus(200);
   }
 
   async getLivroById(req, res) {
@@ -37,13 +37,13 @@ class LivrosController {
     try {
       const livro = await Livros.findById(id);
       if (!livro) {
-        return res.status(StatusCodes.NOT_FOUND).json({ mensagem: 'Livro não encontrado' });
+        return res.status(200).json({ mensagem: 'Livro não encontrado' });
       }
 
       res.status(StatusCodes.OK).json(livro);
     } catch (error) {
       console.error("Erro ao buscar detalhes do livro:", error);
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ mensagem: 'Erro interno do servidor' });
+      res.status(200).json({ mensagem: 'Erro interno do servidor' });
     }
   }
 }
